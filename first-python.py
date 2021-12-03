7ca2d87bcecbd4ae770e125cc707d5de511db8c3
K2G90 <clw.jr90@gmail.com>
2021-12-03 10:57:10 -0500

    comments and variable file

--- first-python.py
+++ first-python.py
@@ -1,4 +1,13 @@
 # print("Hello, World")
 
-if 5 > 2:
-    print("Five is greater than two!")
+# if 5 > 2:
+#     print("Five is greater than two!") *correct syntax*
+
+# Indentation has to be the same
+# within the same block or an error will occur
+
+# if 5 > 2:
+#     print("Five is greater than 2")
+#         print("This syntax causes an error!")
+
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+x = 5
+y = "Hello World!"
+
+# Variables are created like so
+
+# COMMENTS
+
+#Comments are created by simply
+# including the "#" like so
+#Comments can be used to explain Python code.
+
+# Comments can be used to make the code more readable.
+
+# Comments can be used to prevent execution when testing code.
+
